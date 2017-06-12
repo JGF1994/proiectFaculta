0a07ed4f9bfa9189aa4bddade48fab43c6169274
JGF1994 <jurj_gheorghe_florinel@hotmail.com>
2017-06-12 23:12:56 +0300

    Creare functiei capteaza
    Functia capteaza

--- proiect.js
+++ proiect.js
@@ -12,5 +12,14 @@ functia capturaInsucces(stream)
 }
 
 var parametri= {audio: true, video: true};
+
 navigator.mediaDevices.getUserMedia(parametri).then(capturaSucces).catch(capturaInsucces);
 
+
+function capteaza(){
+	var c = document.getElementById("canvas");
+	c.width = video.width;
+	c.height = video.height;
+	var ctx = c.getContext("2d");
+	ctx.drawImage(video,0,0,640,480);
+}
