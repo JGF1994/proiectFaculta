c597772a685d45a08bc203306b6b91e94ec1dde3
JGF1994 <jurj_gheorghe_florinel@hotmail.com>
2017-06-13 22:47:37 +0300

    Creare functie Vibreaza
    Am creeat functia vibreaza() si este apelata in momentul in care se apeleaza la captarea imagini

--- proiect.js
+++ proiect.js
@@ -6,20 +6,25 @@ function capturaSucces(stream)
 {
 	video.srcObject = stream;
 }
-functia capturaInsucces(stream)
+functia capturaInsucces(err)
 {
 	alert("Nu s-a putut captura");
 }
 
-var parametri= {audio: true, video: true};
+var parametri = {audio: true, video: true};
 
 navigator.mediaDevices.getUserMedia(parametri).then(capturaSucces).catch(capturaInsucces);
 
 
 function capteaza(){
+	vibreaza();
 	var c = document.getElementById("canvas");
 	c.width = video.width;
 	c.height = video.height;
 	var ctx = c.getContext("2d");
 	ctx.drawImage(video,0,0,640,480);
+}
+function vibreaza()
+{
+	window.navigator.vibrate(200);
 }
