f74653336b3917cd9e496b8d8512f492e533174a
JGF1994 <jurj_gheorghe_florinel@hotmail.com>
2017-06-16 10:30:10 +0300

    Introducere functie redaSunet si efectAles
    Crearea functiei de redare sunet si crearea efectelor care vor fii aplicate asupra imaginii capturate

--- proiect.js
+++ proiect.js
@@ -1,4 +1,7 @@
 var video = document.getElementById('livestream');
+var sunet=new Audio("sunet.mp3");
+var optiuneAleasa=new int;
+
 if (video)
 	video.addEventListener("touchstart",capturaEcran);
  else
@@ -21,16 +24,29 @@ function vibreaza()
 {
 	window.navigator.vibrate(200);
 }
-function sunetCaptura()
+function redaSunet()
 {
-	sunet=new Audio("sunetcamera");
+	sunet.play();
+}
+function efectAles()
+{
+	if (document.getElementById('').clicked == true)
+		optiuneAleasa=0;
+	if (document.getElementById('').clicked == true)
+		optiuneAleasa=1;
+	if (document.getElementById('').clicked == true)
+		optiuneAleasa=2;
+	if (document.getElementById('').clicked == true)
+		optiuneAleasa=3;	
 }
 function capturaEcran()
 {
 	vibreaza();
+	redaSunet();
 	var c = document.getElementById("canvas");
 	c.width = video.width;
 	c.height = video.height;
 	var ctx = c.getContext("2d");
+	
 	ctx.drawImage(video,0,0,640,480);
 }
