900a4762e0220e4c2594d0e85737f9fcc873da29
JGF1994 <jurj_gheorghe_florinel@hotmail.com>
2017-06-16 15:50:48 +0300

    Functia coloreazaContrast
    Aici am creat functia care modifica contrastul imaginii

--- proiect.js
+++ proiect.js
@@ -77,6 +77,15 @@ function coloreazaSaturare()
 	ctx = canvas.getContext("2d");
 	ctx.drawImage(imagine,0,0,640,480);
 }
+function coloreazaContrast()
+{
+	document.getElementById("canvas").className = "efectblur";
+	var c = document.getElementById("canvas");
+	c.width = imagine.width;
+	c.height = imagine.height;
+	ctx = canvas.getContext("2d");
+	ctx.drawImage(imagine,0,0,640,480);
+}
 
 
 
